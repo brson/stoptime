df9b388b0393da7f5bbd39c4b7104118f04bd7c6
Brian Anderson <andersrb@gmail.com>
2010-04-10 20:37:27 -0400

    Switch to ant and link to scala-library with proguard

---
+++ src/net/negatory/stoptime/StoptimeActivity.scala
@@ -0,0 +1,12 @@
+package net.negatory.stoptime;
+
+import android.app.Activity
+import android.os.Bundle
+
+class StoptimeActivity extends Activity {
+    override def onCreate(savedInstanceState: Bundle)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
