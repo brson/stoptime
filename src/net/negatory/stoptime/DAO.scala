b1ac63b2bcd55773b9f658352ec6d2f7fc2ba1d8
Brian Anderson <andersrb@gmail.com>
2010-05-02 18:59:45 -0400

    Add createFrame DAO method

--- src/net/negatory/stoptime/DAO.scala
+++ src/net/negatory/stoptime/DAO.scala
@@ -8,6 +8,8 @@ class StoptimeOpenHelper(context: Context, dbName: String)
 
   override def onCreate(db: SQLiteDatabase) {
     db.execSQL("create table scene (id integer primary key autoincrement)")
+    db.execSQL("create table frame (id integer primary key autoincrement, sceneId integer, frameData blob, " +
+      "foreign key(sceneId) references scene(id))")
   }
 
   override def onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
@@ -37,7 +39,6 @@ class DAO(context: Context, dbName: String) extends AnyRef with Logging {
     val newSceneId = cursor.getInt(0)
     Log.d("Created new scene with id = " + newSceneId)
     cursor.close
-    db.close
     newSceneId
   }
 
@@ -47,12 +48,21 @@ class DAO(context: Context, dbName: String) extends AnyRef with Logging {
   }
 
   def createFrame(sceneId: Int, imageData: Array[Byte]): Int = {
-    // TODO
-    0
+    // todo need a transaction
+    db.execSQL("insert into frame default values")
+    val cursor = db.rawQuery("select id from frame where rowid=last_insert_rowid()", null)
+    cursor.moveToNext
+    val newFrameId = cursor.getInt(0)
+
+    cursor.close
+    db.execSQL("update frame set sceneId=?, frameData=? where id=?",
+      Array[AnyRef](sceneId: java.lang.Integer, imageData, newFrameId: java.lang.Integer))
+    Log.d("Created new frame for scene " + sceneId + " with id = " + newFrameId)
+    newFrameId
   }
 
-  def loadFrame(sceneId: Int, frameId: Int): Frame = {
+  def loadFrame(frameId: Int, sceneId: Int): Frame = {
     // TODO
-    new Frame(sceneId, frameId, null)
+    new Frame(frameId, sceneId, null)
   }
 }
--- src/net/negatory/stoptime/Frame.scala
+++ src/net/negatory/stoptime/Frame.scala
@@ -2,7 +2,7 @@ package net.negatory.stoptime
 
 import android.graphics.{BitmapFactory, Bitmap}
 
-class Frame(val sceneId: Int, val id: Int, frameData: Array[Byte]) extends AnyRef with Logging {
+class Frame(val id: Int, val sceneId: Int, frameData: Array[Byte]) extends AnyRef with Logging {
 
    val frameBitmap: Bitmap = BitmapFactory.decodeByteArray(frameData, 0, frameData.size)
 }
