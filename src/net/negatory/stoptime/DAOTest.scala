cce80f56f594db0d5ed3cb75a693718a83898002
Brian Anderson <andersrb@gmail.com>
2010-05-02 15:42:47 -0400

    Generate fake frame data for testing

--- src/net/negatory/stoptime/DAOTest.scala
+++ src/net/negatory/stoptime/DAOTest.scala
@@ -1,6 +1,8 @@
 package net.negatory.stoptime
 
 import android.test.{AndroidTestCase}
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
 
 class DAOTest extends AndroidTestCase {
   def testSceneCrud {
@@ -13,8 +15,21 @@ class DAOTest extends AndroidTestCase {
     dao.close
   }
 
+
+  def fakeFrameData: Array[Byte] = {
+    val frameBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888)
+    val memoryStream = new ByteArrayOutputStream()
+    frameBitmap.compress(Bitmap.CompressFormat.JPEG, 50, memoryStream)
+    memoryStream toByteArray
+
+  }
+
   def testFrameCrud {
     val dao = new DAO(getContext, null)
+
+    val sceneId = dao.createScene
+    val frameId = dao.createFrame(sceneId, fakeFrameData)
+    val frame = dao.loadFrame(sceneId, frameId)
 
     dao.close
   }
