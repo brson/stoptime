8679867a719ac9b2632aa29f9b108733e334c312
Brian Anderson <andersrb@gmail.com>
2010-05-31 21:24:58 -0400

    Make this a test project

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,6 @@
 import sbt._
 import java.io.File
 
-class Project(info: ProjectInfo) extends AndroidProject(info) {
+class Project(info: ProjectInfo) extends AndroidTestProject(info) {
   override def androidPlatformName = "android-8"
 }
