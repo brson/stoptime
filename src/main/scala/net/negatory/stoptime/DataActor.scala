9a1ff546dbea8f18b05f97cadc8560a2cddfe6bd
Brian Anderson <andersrb@gmail.com>
2010-06-16 21:32:54 -0400

    Cleanup

--- src/main/scala/net/negatory/stoptime/DataActor.scala
+++ src/main/scala/net/negatory/stoptime/DataActor.scala
@@ -5,37 +5,6 @@ import android.content.Context
 import android.test.AndroidTestCase
 import actors.{TIMEOUT, Actor}
 
-class DataActor(context: Context, dbName: String) extends Actor with Logging {
-
-  start
-  
-  private val dao = new DAO(context, dbName)
-
-  def this(context: Context) {
-    this(context, "stoptime")
-  }
-
-  override def act() {
-    import DataActor._
-    loop {
-      react {
-        case CreateScene =>
-          Log.d("Creating scene")
-          reply(SceneCreated(dao.createScene))
-        case LoadScene(sceneId) => reply(SceneLoaded(dao.loadScene(sceneId)))
-        case LoadAllScenes => actor {
-          loop {
-            
-          }
-        }
-        case Close =>
-          dao.close
-          exit
-      }
-    }
-  }
-}
-
 object DataActor {
   case object Close
   case object Done
@@ -57,10 +26,9 @@ object DataActor {
   case class LoadAllFrames(sceneId: Int)
 }
 
-import junit.framework.Assert._
-
 class DataActorTest extends AndroidTestCase with Logging {
 
+  import junit.framework.Assert._
   import DataActor._, TestTools._
 
   var dataActor: DataActor = null
@@ -92,4 +60,35 @@ class DataActorTest extends AndroidTestCase with Logging {
     
   }
 
+}
+
+class DataActor(context: Context, dbName: String) extends Actor with Logging {
+
+  start
+
+  private val dao = new DAO(context, dbName)
+
+  def this(context: Context) {
+    this(context, "stoptime")
+  }
+
+  override def act() {
+    import DataActor._
+    loop {
+      react {
+        case CreateScene =>
+          Log.d("Creating scene")
+          reply(SceneCreated(dao.createScene))
+        case LoadScene(sceneId) => reply(SceneLoaded(dao.loadScene(sceneId)))
+        case LoadAllScenes => actor {
+          loop {
+
+          }
+        }
+        case Close =>
+          dao.close
+          exit
+      }
+    }
+  }
 }
