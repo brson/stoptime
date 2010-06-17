cf78594b58c15be604794184ac405d0cd3ccc15c
Brian Anderson <andersrb@gmail.com>
2010-06-17 19:07:31 -0400

    Use local build of Scala with Android fixes

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,4 +3,7 @@ import java.io.File
 
 class Project(info: ProjectInfo) extends AndroidTestProject(info) {
   override def androidPlatformName = "android-8"
+
+  val scalaRoot = "/home/brian/Dev/scala"
+  override def localScala = defineScala("2.8.0-local", new File(scalaRoot + "/build/pack")) :: Nil
 }
